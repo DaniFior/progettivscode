5d25fb19cc470ccb4da08d5e3dfc6a9afc3d7c44
DaniFior <fioravantidaniele@icloud.com>
2024-04-17 15:13:52 +0200

    prova upload da casa

---
+++ esercizi_1.py
@@ -0,0 +1,20 @@
+#esercizio 2-3
+name = "EriC"
+print ("Hello {}, can i teach you some python?".format(name))
+
+#esercizio 2-4
+print(str.lower(name))
+print(str.upper(name))
+print(str.title(name))
+
+#esercizio 2-5 e 2-6
+
+author_name = "Steve Jobs"
+quote = "Stay hungry, stay foolish"
+
+print(f"Once {author_name} said: \"{quote}\"")
+
+#esercizio 2-8
+
+filename = "python_notes.txt"
+print(str.removesuffix(filename))
